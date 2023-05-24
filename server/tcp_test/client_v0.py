4f09fa2b57c9b3c41db8549577ea031cae451a0e
gedemais <geoffrey.demaiso@gmail.com>
2023-05-24 17:45:21 +0200

    generation fixed

--- server/tcp_test/client_v0.py
+++ server/tcp_test/client_v0.py
@@ -6,7 +6,7 @@ port = 8080                   # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-moves = ['avance\n', 'droite\n', 'voir\n']
+moves = ['avance\n', 'avance\n', 'droite\n', 'voir\n']
 
 print('connected')
 reply = s.recv(1024).decode('utf-8')
