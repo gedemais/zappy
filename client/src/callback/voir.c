cccb346570bb1ca7dff620a834e9d8f803844a11
Marc-antoine BOYE <maboye@e1r10p6.clusters.42paris.fr>
2023-05-27 12:09:52 +0200

    segfault fix

--- client/src/callback/voir.c
+++ client/src/callback/voir.c
@@ -7,15 +7,12 @@ static int	zappy_client_parse_voir(zappy_client_t *client)
 	int i = 0;
 	int c = 0;
 
-	fprintf(stderr, "%s\n buf: %s\n", __func__, client->buf);
-
 	bzero(client->player.vision_map, VISION_MAP_MAX * CASE_ELEMENTS);
 	client->player.relative_pos = 0;
 	client->player.relative_orientation = 0;
 	if (client->buf[i++] != '{') {
 		r = -1;
 	}
-	fprintf(stderr, "before while\n");
 	while (r == 0 && i < (int)strlen((char*)client->buf))
 	{
 		if (client->buf[i] == '}') {
@@ -27,16 +24,16 @@ static int	zappy_client_parse_voir(zappy_client_t *client)
 		else if (client->buf[i] != ' ')
 		{
 			bool b = false;
-			for (int j = 0 ; case_ressources[j] ; j++) {
-				if (!memcmp(case_ressources[j],
-							(char*)&client->buf[i], strlen(case_ressources[j]))) {
+			for (int j = 0; j < R_MAX; j++) {
+				if (case_ressources[j] == NULL)
+					break ;
+				if (!memcmp(case_ressources[j], (char*)&client->buf[i], strlen(case_ressources[j]))) {
 					b = true;
 					client->player.vision_map[c * CASE_ELEMENTS + j]++;
 					i += (int)strlen(case_ressources[j]);
 				}
 			}
-			if (b == false)
-			{
+			if (b == false) {
 				r = -1;
 			}
 		}
@@ -67,11 +64,9 @@ int			zappy_voir_cb(zappy_client_t *client)
 {
 	int r = 0;
 
-	fprintf(stderr, "%s\n", __func__);
 	r = zappy_client_parse_voir(client);
 	if (r == 0)
 	{
-		fprintf(stderr, "r == 0\n");
 		client->task = PLAYER_TASK_LOOT;
 		zappy_debug_print_vision_map(client);
 		client->player.relative_pos = 0;
