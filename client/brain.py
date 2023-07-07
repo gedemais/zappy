90a2e417b621e30d5bc5b683842c07f6fcc744f1
gedemais-formations <demaison.geoffrey@gmail.com>
2023-07-07 15:36:50 +0200

    let's merge again

--- client/brain.py
+++ client/brain.py
@@ -13,11 +13,15 @@ class L(Enum):
 	LOOT_THYSTAME = 6
 
 class Brain:
+    # Explorated tiles
     explored_tiles = []
     view = []
     loot_priorities = [5, 4, 3, 3, 2, 2, 4]
+    # Position in the bot's referential
     x, y = 0, 0
-    orientation = 0
+    # Tile index in view's tiles referential (amount depending on bot level)
+    tile = 0
+    # Has the player ever seen ?
     blind = True
 
     def __init__(self):
@@ -39,34 +43,40 @@ class Brain:
 
     def loot(self, client):
         commands = []
-        viewed = False
 
-        if viewed == False and (len(self.view) == 0 or self.view_size == len(self.explored_tiles)):
+        if self.blind and len(self.view) == 0:
             commands.append(C.DROITE if randint(0, 100) < 50 else C.GAUCHE)
             commands.append(C.VOIR)
-            viewed = True
+            self.blind = False
 
-        if viewed and len(client.qreceive.buf) > 0:
+        if not self.blind and len(client.qreceive.buf) > 0:
             response = client.qreceive.buf
 
-            if response[-1][0] == '{' and response[-1][-1] == '}':
-                viewed = False
+            if response[-1][0] == '{' and response[-1][-1] == '}' and 'ttl' not in response:
+                self.blind = False
                 response = client.qreceive.buf[-1][1:-1]
-                print(response)
                 for tile in response.split(','):
-                    self.view.append([loot for loot in tile.split(' ')].remove(''))
+                    loot = tile.split(' ')
+                    while '' in loot:
+                        loot.remove('')
+                    self.view.append(loot)
+
                 self.view_size = len(self.view)
-                print(self.view)
-                print(self.view_size)
-        #
-        #if (x, y) in explored_tiles:
-        #    return
-        #
-        # if len(view[0] > 0):
+        
+        if (self.x, self.y) in self.explored_tiles:
+            # Move it on the next tile of its view
+            pass
+
+        #if len(view[self.tile]) > 0: # and not busy
+        #    if 
+        #    commands.append(id=C.TAKE, buf='')
+
+        #    pass
         #   Ramasser les ressources interessantes sur la case en fonction des prios
-        #   explored_tiles.append((x, y))
+        #   explored_tiles.append((self.x, self.y))
         #   Progresser dans la view
 
         for i, c in enumerate(commands):
+            print(c)
             commands[i] = Command(id = c)
         return commands
