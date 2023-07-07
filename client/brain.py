6b0f119cc3452a607c996a41ccef7803bbd1001a
gedemais-formations <demaison.geoffrey@gmail.com>
2023-07-07 14:40:13 +0200

    changes bug

--- client/brain.py
+++ client/brain.py
@@ -1,6 +1,7 @@
 from transitions import Machine
 from enum import Enum
 from command import C, Command
+from random import randint
 
 class L(Enum):
 	LOOT_FOOD = 0
@@ -20,26 +21,42 @@ class Brain:
 
     def __init__(self):
         self.machine = Machine(model=self, states=["IDLE", "POPULATE", "LOOT"], initial="IDLE")
-        self.machine.add_transition("go_loot", "IDLE", "LOOT")
+        self.machine.add_transition("fetch", "IDLE", "LOOT")
         self.machine.add_transition("stop", "LOOT", "IDLE")
 
-    def update(self):
+    def update_state(self, client):
+        pass
+
+    def update(self, client):
         if self.state == "LOOT":
-            return self.loot()
+            return self.loot(client)
         elif self.state == "IDLE":
             return
 
     def populate(self):
         print("toto")
 
-    def loot(self):
-         return [
-              Command(id = C.VOIR)
-		 ]
-        #if len(view) == 0 or self.view_size == len(view):
-        #   Randomize orientation
-        #   Launch view command and parse result in an array of arrays
-        #   self.view_size = len(view)
+    def loot(self, client):
+        commands = []
+        viewed = False
+
+        if viewed == False and (len(self.view) == 0 or self.view_size == len(self.explored_tiles)):
+            commands.append(C.DROITE if randint(0, 100) < 50 else C.GAUCHE)
+            commands.append(C.VOIR)
+            viewed = True
+
+        if viewed and len(client.qreceive.buf) > 0:
+            response = client.qreceive.buf
+
+            if response[-1][0] == '{' and response[-1][-1] == '}':
+                viewed = False
+                response = client.qreceive.buf[-1][1:-1]
+                print(response)
+                for tile in response.split(','):
+                    self.view.append([loot for loot in tile.split(' ')].remove(''))
+                self.view_size = len(self.view)
+                print(self.view)
+                print(self.view_size)
         #
         #if (x, y) in explored_tiles:
         #    return
@@ -48,3 +65,7 @@ class Brain:
         #   Ramasser les ressources interessantes sur la case en fonction des prios
         #   explored_tiles.append((x, y))
         #   Progresser dans la view
+
+        for i, c in enumerate(commands):
+            commands[i] = Command(id = c)
+        return commands
--- client/main.py
+++ client/main.py
@@ -5,27 +5,29 @@ from client import Client
 from IA import IA
 from brain import Brain
 
+from time import sleep
+
 
 host = "localhost"
 port = 8080
 team_name = "foo"
 
 def	parse_arguments():
-	arguments = sys.argv
+    arguments = sys.argv
 
 if __name__ == "__main__":
 
-	parse_arguments()
+    parse_arguments()
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client = Client(host, port, team_name, s)
-	client.connect()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client = Client(host, port, team_name, s)
+    client.connect()
 
-	bernard = IA()
-	brain = Brain()
-	brain.go_loot()
-
-	while client.bot.alive == True:
-		bernard.interact(brain.update())
-		client.transceive(bernard.cmd)
-		client.receive(bernard.cmd)
+    bernard = IA()
+    brain = Brain()
+    brain.fetch()
+    while client.bot.alive == True:
+        brain.update_state(client)
+        bernard.interact(brain.update(client))
+        client.transceive(bernard.cmd)
+        client.receive(bernard.cmd)
